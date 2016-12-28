926b7fcef49c29c5c3123b68d923b83f52f732cb
Peter Velkov <kidroca@gmail.com>
2016-12-28 11:31:43 +0200

    feat(app.component): updated with todo functionality
    Completed stage 1 of the `https://www.sitepoint.com/angular-2-tutorial/` tutorial

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,34 +1,40 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {TestBed, async, ComponentFixture} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+import {FormsModule} from '@angular/forms';
+import {Todo} from './todo';
+
+let fixture: ComponentFixture<AppComponent>;
 
 describe('AppComponent', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                FormsModule
+            ],
+            declarations: [
+                AppComponent
+            ],
+        });
+        TestBed.compileComponents();
     });
-    TestBed.compileComponents();
-  });
 
-  it('should create the app', async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
+    beforeEach(() => fixture = TestBed.createComponent(AppComponent));
 
-  it(`should have as title 'app works!'`, async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
+    it('should create the app', async(() => {
+        let app = fixture.debugElement.componentInstance;
+        expect(app).toBeTruthy();
+    }));
 
-  it('should render title in a h1 tag', async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    let compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
-  }));
+    it(`should have as title 'app works!'`, async(() => {
+        let app = fixture.debugElement.componentInstance;
+        expect(app.newTodo).toEqual(jasmine.any(Todo));
+    }));
+
+    it('should render title in a h1 tag', async(() => {
+        fixture.detectChanges();
+        let compiled = fixture.debugElement.nativeElement;
+        expect(compiled.querySelector('h1').textContent).toContain('Todos');
+    }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TodoDataService} from './todo-data.service';
+import {Todo} from './todo';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    providers: [TodoDataService],
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+    newTodo: Todo = new Todo();
+
+    constructor(private todoDataService: TodoDataService) {}
+
+    addTodo() {
+        this.todoDataService.add(this.newTodo);
+        this.newTodo = new Todo();
+    }
+
+    toggleTodoComplete(todo: Todo) {
+        this.todoDataService.toggleTodoComplete(todo);
+    }
+
+    removeTodo(todo: Todo) {
+        this.todoDataService.deleteById(todo.id);
+    }
+
+    get todos() {
+        return this.todoDataService.getAll();
+    }
 }
